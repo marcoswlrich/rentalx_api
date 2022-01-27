e5220c625f1fefe76494b33c8b7868a548f1b0b7
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-01-27 00:12:45 -0300

    Inserido ID com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, descrition } = request.body;
 
-  categories.push({
+  const category = {
     name,
     descrition,
-  });
+    id: uuidV4(),
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
