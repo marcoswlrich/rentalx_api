c25a314838fb200e1dd812435fe698e34312164b
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-31 01:58:07 -0300

    Tratamento de erros

--- src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
@@ -20,7 +21,7 @@ class CreateUserUseCase {
     const userArleadyExists = await this.usersRepository.findByEmail(email);
 
     if (userArleadyExists) {
-      throw new Error('User arleady registered');
+      throw new AppError('User arleady registered');
     }
     const passwordHash = await hash(password, 8);
     await this.usersRepository.create({
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ISpecificationRepository } from '../../repositories/ISpecificationRepository';
 
 interface IRequest {
@@ -17,7 +18,7 @@ class CreateSpecificationUseCase {
       await this.specificationsRepository.findByname(name);
 
     if (specificationAlreadyExists) {
-      throw new Error('Specification already exists!');
+      throw new AppError('Specification already exists!');
     }
     await this.specificationsRepository.create({
       name,
