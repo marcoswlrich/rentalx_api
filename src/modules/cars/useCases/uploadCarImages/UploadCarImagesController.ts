a4ba2b0c075ec3f9d68567d1c1f40a0ace54e006
marcoswlrich <marcosapw.dev@gmail.com>
2022-06-07 01:39:53 -0300

    uso do cadastro de imagens do carro

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesController.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesController.ts
@@ -1,3 +1,28 @@
-class UploadCarImagesUseCase {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
-export { UploadCarImagesUseCase };
+import { UploadCarImagesUseCase } from './UploadCarImageUseCase';
+
+interface IFiles {
+  filename: string;
+}
+
+class UploadCarImagesController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const images = request.files as IFiles[];
+
+    const uploadCarImagesUseCase = container.resolve(UploadCarImagesUseCase);
+
+    const images_name = images.map(file => file.filename);
+
+    await uploadCarImagesUseCase.execute({
+      car_id: id,
+      images_name,
+    });
+
+    return response.status(201).send();
+  }
+}
+
+export { UploadCarImagesController };
