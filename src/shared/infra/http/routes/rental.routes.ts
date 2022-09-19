cd018816db5945e3c80857b03ed2f9ae22bc83f6
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-19 00:34:14 -0400

    Update rotes

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,20 +1,28 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
-// import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
-// import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
-// const devolutionRentalController = new DevolutionRentalController();
-// const listRentalsByUserController = new ListRentalsByUserController();
+const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
-rentalRoutes.post('/devolution/:id', ensureAuthenticated);
+rentalRoutes.post(
+  '/devolution/:id',
+  ensureAuthenticated,
+  devolutionRentalController.handle,
+);
 
-rentalRoutes.get('/user', ensureAuthenticated);
+rentalRoutes.get(
+  '/user',
+  ensureAuthenticated,
+  listRentalsByUserController.handle,
+);
 
 export { rentalRoutes };
