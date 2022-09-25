bb9567c1eb0155c08f7cc8c1bb70359c4cf95249
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-25 03:06:34 -0400

    Config de autorizacao Usuario

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'd09a2e4605c5a9d13f8f0fab010a1267',
+  expires_in_token: '15m',
+  secret_refresh_token: '03fb4ee11176893dccc5343823606dfb',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
