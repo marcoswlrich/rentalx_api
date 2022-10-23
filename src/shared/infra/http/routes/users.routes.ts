244b77fe569fa33bc139ba6814a098f1bb16d24b
marcoswlrich <marcosapw.dev@gmail.com>
2022-10-23 14:16:56 -0400

    uploadConfig Update

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,16 +3,17 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/CreateUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUserUseCase/ProfileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig);
+
 const createUserController = new CreateUserController();
-
 const updateUserAvatarController = new UpdateUserAvatarController();
-
-const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('/', createUserController.handle);
 
@@ -22,5 +23,7 @@ usersRoutes.patch(
   uploadAvatar.single('avatar'),
   updateUserAvatarController.handle,
 );
+
+usersRoutes.get('/profile', ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
