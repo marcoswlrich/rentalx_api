4f58bfe0798b9967c272e0c938fb04dbe55c90b1
marcoswlrich <marcosapw.dev@gmail.com>
2022-10-23 14:20:20 -0400

    Upload rotes img Cars e Avatar

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import 'express-async-errors';
 import '@shared/container';
-
+import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
 
 import swaggerFile from '../../../swagger.json';
@@ -16,20 +17,21 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-    return response.status(500).json({
-      status: 'Error',
-      message: `Internal server error -  ${err.message}`,
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
     });
-  },
-);
+  }
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error -  ${err.message}`,
+  });
+});
 
 export { app };
