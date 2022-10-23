e2344d621b266a6fdd95480fb44046f9a5ca7ab5
marcoswlrich <marcosapw.dev@gmail.com>
2022-10-23 14:56:51 -0400

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
@@ -20,6 +21,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response) => {
